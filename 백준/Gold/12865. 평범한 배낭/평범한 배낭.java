d3b72815b01da18cec8f0e91e419f21c831c9e68
김형균 <97081506+HyeuongKyun@users.noreply.github.com>
2024-07-10 08:52:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 51328 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,35 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(inputs[0]);
-        int K = Integer.parseInt(inputs[1]);
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] dpArr = new int[N+1][K+1]; //무게따라 표현
+        int[][] items = new int[N+1][2]; //물건 정보
 
-        int[][] item = new int[N + 1][2];  // weight, value
-
-        for (int i = 1; i <= N; i++) {
-            inputs = br.readLine().split(" ");
-            item[i][0] = Integer.parseInt(inputs[0]);
-            item[i][1] = Integer.parseInt(inputs[1]);
+        for(int i=1;i<=N;i++){
+            st = new StringTokenizer(br.readLine());
+            items[i][0]=Integer.parseInt(st.nextToken());
+            items[i][1]=Integer.parseInt(st.nextToken());
         }
 
-        int[][] dp = new int[N + 1][K + 1];
+        dp(N,K,dpArr, items);
 
-        for (int k = 1; k <= K; k++) { // 무게
-            for (int i = 1; i <= N; i++) { // item
-                dp[i][k] = dp[i - 1][k];
-                if (k - item[i][0] >= 0) {
-                    dp[i][k] = Math.max(dp[i - 1][k], item[i][1] + dp[i - 1][k - item[i][0]]);
+    }
+
+    public static void dp(int N, int K, int[][] dpArr, int[][] items){
+        for(int n=1;n<=N;n++){
+
+            for(int k=1;k<=K;k++){
+                if(k-items[n][0]<0) {
+                    dpArr[n][k] = dpArr[n-1][k];
+                    continue;
                 }
+                dpArr[n][k] = Math.max(dpArr[n-1][k],dpArr[n-1][k-items[n][0]]+items[n][1]);
             }
+
         }
+        System.out.println(dpArr[N][K]);
 
-
-        System.out.println(dp[N][K]);
     }
 }
