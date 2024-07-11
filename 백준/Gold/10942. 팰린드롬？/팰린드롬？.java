977073db14eb1653fd64d71bbcaecaf9ab359e62
김형균 <97081506+HyeuongKyun@users.noreply.github.com>
2024-07-11 13:51:57 +0900

    [Gold IV] Title: 팰린드롬?, Time: 732 ms, Memory: 245444 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -4,9 +4,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
@@ -15,33 +14,46 @@ public class Main {
         int[] nArr = new int[N+1];
         for(int n=1;n<=N;n++) nArr[n] = Integer.parseInt(st.nextToken());
 
+        //0이면 안해본거, 1이면 팰린드롬o, 2면 팰린드롬x
         int[][] nMap = new int[N+1][N+1];
         nMap[N][N]=1;
-        for(int i=N;i>=1;i--){
-            for(int j=i;j<=N;j++){
-                //하나는 팰린드롬
-                if(j==i) nMap[i][j]=1;
-                //두개는 앞뒤 비교
-                else if(j==i+1 && nArr[i]==nArr[j]) nMap[i][j]=1;
-                // 3개 부터는 nArr[i]==nArr[j]이고 nMap[i+1][j-1]==1이면 nMap[i][j]=1
-                else if(nMap[i+1][j-1]==1 && nArr[i]==nArr[j]) nMap[i][j]=1;
-            }
+        for(int n=1;n<N;n++){
+            //하나는 팰린드롬
+            nMap[n][n]=1;
+            //두개는 앞뒤 비교
+            if(nArr[n]==nArr[n+1]) nMap[n][n+1]=1;
+            else nMap[n][n+1]=2;
+            // 3개 부터는 nArr[i]==nArr[j]이고 nMap[i+1][j-1]==1이면 nMap[i][j]=1
         }
 
         st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
-        StringBuilder sb = new StringBuilder();
-        for(int m=0;m<M;m++){
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
 
-            sb.append(nMap[i][j]).append("\n");
+        for(int m=1;m<=M;m++){
+            st = new StringTokenizer(br.readLine());
+            int idx1 = Integer.parseInt(st.nextToken());
+            int idx2 = Integer.parseInt(st.nextToken());
+            int result = findVal(idx1, idx2, nMap,nArr);
+            if(result==2) result=0;
+            sb.append(result).append("\n");
         }
 
-        System.out.println(sb);
+        System.out.println(sb.toString());
+
     }
 
+
+    public static int findVal(int idx1, int idx2, int[][] nMap, int[] nArr){
+        if(nMap[idx1][idx2]!=0) {
+            return nMap[idx1][idx2];
+        }
+
+        int result = findVal(idx1+1, idx2-1,nMap ,nArr);
+        if(result==1 && nArr[idx1]==nArr[idx2]) nMap[idx1][idx2]=1;
+        else nMap[idx1][idx2]=2;
+        return nMap[idx1][idx2];
+    }
+
+
+
 }
-
-
