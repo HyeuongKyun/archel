41c9e993118adc4903f77049fa894eab247c9288
김형균 <97081506+HyeuongKyun@users.noreply.github.com>
2024-07-11 13:54:59 +0900

    [Gold IV] Title: 팰린드롬?, Time: 728 ms, Memory: 244912 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -7,53 +7,45 @@ public class Main {
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine().trim());
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-        int[] nArr = new int[N+1];
-        for(int n=1;n<=N;n++) nArr[n] = Integer.parseInt(st.nextToken());
-
-        //0이면 안해본거, 1이면 팰린드롬o, 2면 팰린드롬x
-        int[][] nMap = new int[N+1][N+1];
-        nMap[N][N]=1;
-        for(int n=1;n<N;n++){
-            //하나는 팰린드롬
-            nMap[n][n]=1;
-            //두개는 앞뒤 비교
-            if(nArr[n]==nArr[n+1]) nMap[n][n+1]=1;
-            else nMap[n][n+1]=2;
-            // 3개 부터는 nArr[i]==nArr[j]이고 nMap[i+1][j-1]==1이면 nMap[i][j]=1
+        int[] nArr = new int[N + 1];
+        for (int n = 1; n <= N; n++) {
+            nArr[n] = Integer.parseInt(st.nextToken());
         }
 
-        st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st.nextToken());
+        int[][] memo = new int[N + 1][N + 1];
 
-        for(int m=1;m<=M;m++){
+        int M = Integer.parseInt(br.readLine().trim());
+        for (int m = 0; m < M; m++) {
             st = new StringTokenizer(br.readLine());
-            int idx1 = Integer.parseInt(st.nextToken());
-            int idx2 = Integer.parseInt(st.nextToken());
-            int result = findVal(idx1, idx2, nMap,nArr);
-            if(result==2) result=0;
-            sb.append(result).append("\n");
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            int result = isPalindrome(S, E, nArr, memo);
+            if(result==2) result = 0;
+            sb.append(result).append('\n');
         }
 
         System.out.println(sb.toString());
-
     }
 
-
-    public static int findVal(int idx1, int idx2, int[][] nMap, int[] nArr){
-        if(nMap[idx1][idx2]!=0) {
-            return nMap[idx1][idx2];
+    public static int isPalindrome(int start, int end, int[] arr, int[][] memo) {
+        if (start == end) {
+            return 1; // 길이가 1인 경우
+        }
+        if (start + 1 == end) {
+            return arr[start] == arr[end] ? 1 : 2; // 길이가 2인 경우
+        }
+        if (memo[start][end] != 0) {
+            return memo[start][end]; // 이미 계산된 경우
         }
 
-        int result = findVal(idx1+1, idx2-1,nMap ,nArr);
-        if(result==1 && nArr[idx1]==nArr[idx2]) nMap[idx1][idx2]=1;
-        else nMap[idx1][idx2]=2;
-        return nMap[idx1][idx2];
+        if (arr[start] == arr[end] && isPalindrome(start + 1, end - 1, arr, memo) == 1) {
+            memo[start][end] = 1;
+        } else {
+            memo[start][end] = 2;
+        }
+        return memo[start][end];
     }
-
-
-
 }
