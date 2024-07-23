95b0449c79a0c837bc0a1df7794bcc85eb584f2a
김형균 <97081506+HyeuongKyun@users.noreply.github.com>
2024-07-23 10:52:34 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 104 ms, Memory: 12392 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -1,49 +1,59 @@
-import javax.annotation.PostConstruct;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken()) ;
+        int[] arr = new int[N];
+        int[] order = new int[N];
+        int[] lisArray = new int[N];
+        int size = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int maxCnt = 0;
-        int maxIdx = -1;
-        int[][] arr = new int[N][2];
-        List<List<Integer>> list = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
-        for(int n=0;n<N;n++) {
-            arr[n][0] = Integer.parseInt(st.nextToken());
-            list.add(new ArrayList<>());
-            int tempInt = 0;
-            for(int m=0;m<n;m++){
-                if(arr[m][0]<arr[n][0] && arr[m][1]>tempInt) {
-                    tempInt=arr[m][1];
-                    list.remove(n);
-                    list.add(new ArrayList<>());
-
-                    List<Integer> tempList = list.get(m);
-                    for(int i=0;i<tempList.size();i++) list.get(n).add(tempList.get(i));
-                }
+        lisArray[0] = arr[0] = Integer.parseInt(st.nextToken());
+        for(int n=1;n<N;n++){
+            arr[n]=Integer.parseInt(st.nextToken());
+            if(arr[n]>lisArray[size]) {
+                lisArray[++size]=arr[n];
+                order[n] = size;
+            } else {
+                int idx = binarySearch(arr[n], size, lisArray);
+                lisArray[idx] = arr[n];
+                order[n] = idx;
             }
-
-            if(tempInt+1>maxCnt) {
-                maxCnt = tempInt+1;
-                maxIdx = n;
-            }
-
-            arr[n][1] = tempInt+1;
-            list.get(n).add(arr[n][0]);
         }
-        System.out.println(maxCnt);
-        for(int i=0;i<list.get(maxIdx).size();i++) System.out.printf("%d ",list.get(maxIdx).get(i));
 
+        StringBuilder sb = new StringBuilder();
+        sb.append(size+1).append("\n");
+        Stack<Integer> stack = new Stack<>();
+        for(int i=N-1;i>=0;i--){
+            if(order[i]==size) {
+                stack.add(arr[i]);
+                size--;
+            } else if (size==-1) break;
+        }
 
+        int len = stack.size();
+        for(int i=0;i<len;i++)
+            sb.append(stack.pop()).append(" ");
+
+        System.out.println(sb.toString());
     }
 
+    public static int binarySearch(int arrVal, int size, int[] lisArray){
+        int left = 0;
+        int right = size;
+        while (left<right){
+            int mid = (left+right)/2;
+            for(int i=0;i<lisArray.length;i++)
+            if(arrVal>lisArray[mid]) left = mid+1;
+            else right = mid;
+        }
+        return right;
+    }
 }
